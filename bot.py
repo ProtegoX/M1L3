4730ee2af96a89b5a856c87801dcf84a1d8fc753
ProtegoX <puhovmatvej@gmail.com>
2025-08-16 14:32:11 +0200

    new feature: ban statement

--- bot.py
+++ bot.py
@@ -1,26 +1,40 @@
-import telebot # библиотека telebot
-from config import token # импорт токена
+import telebot # telebot lib
+from config import token # importing token
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом.")
+    bot.reply_to(message, "Hi, I am ChatMod bot")
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
+    if message.reply_to_message: 
+        chat_id = message.chat.id 
         user_id = message.reply_to_message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+
         if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно забанить администратора.")
+            bot.reply_to(message, "You can't ban and admin")
         else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
-            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+            bot.ban_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"user @{message.reply_to_message.from_user.username} was blocked.")
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+        bot.reply_to(message, "you must call the ban statement in reply to the message you want to ban.")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if "https://" == message.text[:8]:
+        
+        chat_id = message.chat.id 
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+        if user_status != 'administrator' or user_status != 'creator':
+            bot.ban_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"user @{message.reply_to_message.from_user.username} was blocked.")
+    
+    else:    
+        bot.reply_to(message, message.text) 
 
 bot.infinity_polling(none_stop=True)
